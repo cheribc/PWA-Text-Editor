d2a1133f77e9ada1f8c28fea454fb51ed226ab19
Heather Cooper <90538387+cheribc@users.noreply.github.com>
2022-03-21 15:20:48 -0400

    added logic to editor.js file

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,38 +13,42 @@ const initdb = async () =>
         },
     });
 
-// Add logic to a method that accepts some content and adds it to the database
+// Method that accepts some content and adds it to the IndexedDB database using the idb module
 export const putDb = async (content) => {
     
-    console.error('putDb not implemented');
+    console.log('PUT to the database');
     // connect to DB and version we want to use
     const jateDb = await openDB('jate', 1);
     // make new transaction...need to specify the DB we are posting to and the data privileges. 
     const tx = jateDb.transaction('jate', 'readwrite');
     // open the object store
-    const objStore = tx.objectStore('jate');
+    const store = tx.objectStore('jate');
     // use the .add() method to pass in content
-    const request = objStore.put({ id: id, value: value })
+    const request = store.put({ id: 1, value: content });
     // confirm the data was added
-    const res = await req;
-    console.log('data saved to the jateDB', res);
-    };
+    const result = await request;
+    console.log('🚀 - data saved to the database', result.value);
+};
 
-//Add logic for a method that gets all the content from the database
+// Method that gets all the content from the database
 export const getDb = async () => {
-    console.error('getDb not implemented');
+    console.log('GET from the database');
 
     // connect to DB and version we want to use
     const jateDb = await openDB('jate', 1);
     // make new transaction...need to specify the DB we are posting to and the data privileges. 
-    const tx = jateDb.transaction('jate', 'readwrite');
+    const tx = jateDb.transaction('jate', 'readonly');
     // open the object store
     const objStore = tx.objectStore('jate');
-    // use the .getAll() method to grab all the content in the DB
-    const req = objStore.getAll()
-    // confirm the data was fetched
-    const res = await req;
-    console.log('data saved to the jateDB', res);
+    // use the .get() method to grab specified content in the DB
+    const request = store.get(1);
+    // confirm that the data was fetched
+    const result = await request;
+    result
+        ? console.log('🚀 - data retrieved from the database', result.value)
+        : console.log('🚀 - data not found in the database');
+        // Optional Chaining-Check to see if a variable is defined and return it if it is
+        return result?.value;
     };
 
 
